aa9b7955920b67c87bf83b94198bb6652e7bc951
Soumya Samanvaya <soumya.samanvaya@thoughtworks.com>
2023-08-14 16:24:47 +0530

    Changes made by thunks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
 
-const el = document.getElementById("root");
+const el = document.getElementById('root');
 const root = createRoot(el);
 
 root.render(
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { usersReducer } from "./slices/usersSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { usersReducer } from './slices/usersSlice';
 
 export const store = configureStore({
-    reducer:{
-        users:usersReducer
-    },
+  reducer: {
+    users: usersReducer,
+  },
 });
+
+export * from './thunks/fetchUsers';
+export * from './thunks/addUser';
+export * from './thunks/removeUser';
